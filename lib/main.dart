8d6f5e867dd5cb7ffa75642df5d4a801a0f6e38b
Eduardo0515 <183382@ids.upchiapas.edu.mx>
2021-06-14 11:15:26 -0500

    Tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prueba_cerrada/src/pages/tab.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: "Hello",),
+      home: TabView()
     );
   }
 }
